7deb3edb279d348b34dc200ee16da438524bfce9
Nick(Zhixin) Ni <Nick.Ni@autodesk.com>
2019-10-15 17:41:42 +0800

    add 反序数

--- Python Exercises.py
+++ Python Exercises.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
-
+'''
 #求阶乘n!
 def factorial(num):
     result = 1
     for n in range(1,num+1):
         result*=n
     return result
-"""test code
+#test code
 n=int(input('n= '))
 print (factorial(n))
-"""
 
 #英寸与厘米互换
 def inch_cm(length,unit):
@@ -19,11 +18,11 @@ def inch_cm(length,unit):
         print('%.2f厘米=%.2f英寸' %(length, length/2.54))
     else:
         print('请输入有效的单位')
-'''test code:输入单位时需要加引号(Python版本2)
+#test code:输入单位时需要加引号(Python版本2)
 length = float(input('请输入长度 '))
 unit = str(input('请输入单位 '))
 inch_cm(length,unit)
-'''
+
 
 #分数转换成等级
 def score_grade(score):
@@ -38,10 +37,9 @@ def score_grade(score):
     else:
         grade = 'E'
     print ('你的成绩为：', grade)
-'''test code
+#test code
 score = float(input('输入你的分数'))
 score_grade(score)
-'''
 
 #猜数字游戏
 import random
@@ -60,9 +58,8 @@ def guess_number():
             break
     if counter >7:
         print ('你的智商堪忧啊')
-'''test code
+#test code
 guess_number()
-'''
 
 #九九乘法表
 def multiply99(number):
@@ -70,9 +67,8 @@ def multiply99(number):
         for j in range(1,i+1):
             print ('%dx%d=%d' %(i,j,i*j), end='\t')
         print()
-'''test code
+#test code
 multiply99(9)
-'''
 
 #判断质数
 import math
@@ -87,10 +83,9 @@ def prime(number):
         print ('%d是素数' % number)
     else:
         print ('%d不是素数' % number)
-'''test code
+#test code
 number=int(input('输入一个正整数：'))
 prime(number)
-'''
 
 #求两个数的最大公约数和最小公倍数
 def divisor_multiple(x,y):
@@ -101,11 +96,9 @@ def divisor_multiple(x,y):
             print ('%d和%d的最大公约数是%d' % (x,y,factor))
             print ('%d和%d的最小公倍数是%d' % (x,y,(x*y)//factor))
             break
-'''
 x= int(input('第一个正整数'))
 y= int(input('第二个正整数'))
 divisor_multiple(x,y)
-'''
 
 #打印图形
 def stars(row):
@@ -124,6 +117,25 @@ row=int(input('要几行：'))
 stars(row)
 
 
+#多个数相加
+def add(*args):
+    total = 0
+    for i in args:
+        total+=i
+    return total
+print (add(5,6,7,8,9))
+'''
+
+#创建反序正整数
+def reorder(num):
+    temp=0
+    while num >0:
+        temp = temp * 10 + num%10
+        num=num//10
+    return temp
+num = int(input('请输入一个正整数'))
+temp = reorder(num)
+print('%d的反序数是%d' %(num,temp))
 
 
 

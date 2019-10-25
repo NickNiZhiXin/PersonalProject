b75b27c98245903fbe7203ee717ab2c88c6d7996
NickNiZhiXin <32223498+NickNiZhiXin@users.noreply.github.com>
2019-10-25 17:33:11 +0800

    temp

--- temp.py
+++ temp.py
@@ -1,39 +1,12 @@
-#显示时间
-from time import time,localtime,sleep
-import os
-class Clock(object):
-  def __init__(self, hour=0,minute=0,second=0):
-    self._hour = hour
-    self._minute = minute
-    self._second = second
-
-  @classmethod
-  def now(cls):
-    ctime = localtime(time())
-    return cls(ctime.tm_hour,ctime.tm_min,ctime.tm_sec)
-
-  def run(self):
-    self._second+=1
-    if self._second==60:
-      self._second=0
-      self._minute+=1
-      if self._minute==60:
-        self._minute=0
-        self._hour+=1
-        if self._hour==24:
-          self._hour=0
-  def show(self):
-    return '%02d:%02d:%02d' %(self._hour, self._minute, self._second)
-def main():
-  clock = Clock.now()
-  while True:
-    os.system("clear")
-    print(clock.show())
-    sleep(1)
-    clock.run()
-
-if __name__=='__main__':
-  main()
+#求阶乘n!
+def factorial(num):
+    result = 1
+    for n in range(1,num+1):
+        result*=n
+    return result
+#test code
+n=int(input('n= '))
+print (factorial(n))
 
 
 

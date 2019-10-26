56b2bd95a08d48ef004e60606d8d668e465d95b3
NickNiZhiXin <32223498+NickNiZhiXin@users.noreply.github.com>
2019-10-26 20:19:52 +0800

    add one case

--- temp.py
+++ temp.py
@@ -1,12 +1,26 @@
-#求阶乘n!
-def factorial(num):
-    result = 1
-    for n in range(1,num+1):
-        result*=n
-    return result
-#test code
-n=int(input('n= '))
-print (factorial(n))
+
+#计算1000内所有的完数：一个数等于它的因子之和
+from sys import stdout
+
+for j in range(2,1000):
+  k=[]
+  n=-1
+  s=j
+  for i in range (1,j):
+    if j%i == 0:
+      n +=1
+      s -=i
+      k.append(i)
+  if s == 0:
+    print (j)
+    for i in range(n):
+      stdout.write(str(k[i]))
+      stdout.write(' ')
+    print (k[n])
+
+
+
+
 
 
 

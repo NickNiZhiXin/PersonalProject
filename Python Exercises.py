4c2d3793a5aadf6988795f915bc9f517efb2c5be
NickNiZhiXin <32223498+NickNiZhiXin@users.noreply.github.com>
2019-11-01 11:00:10 +0800

    11

--- Python Exercises.py
+++ Python Exercises.py
@@ -489,9 +489,8 @@ class DownloadHanlder(Thread):
     def run(self):
         filename = self.url[self.url.rfind('/') + 1:]
         resp = requests.get(self.url)
-        with open ('/Downloads/' + filename, 'wb') as f:
+        with open ('/Users/nizhixin/Documents/' + filename, 'wb') as f:
             f.write(resp.content)
-
 
 def main():
     # 通过requests模块的get函数获取网络资源
@@ -512,4 +511,59 @@ def main():
 if __name__ == '__main__':
     main()
 
+#把数字写到文件里
+from math import sqrt
 
+def is_prime(n):
+    assert n >0
+    for factor in range(2, int(sqrt(n))):
+        if n % factor == 0:
+            return False
+    return True if n!= 1 else False
+
+def main():
+    filenames = ('a.txt','b.txt','c.txt')
+    fs_list = []
+    try:
+        for filename in filenames:
+            fs_list.append(open(filename, 'w', encoding = 'utf-8'))
+        for number in range (1,10000):
+            if is_prime(number):
+                if number < 100:
+                    fs_list[0].write(str(number) + '\n')
+                elif number < 1000:
+                    fs_list[1].write(str(number) + '\n')
+                else:
+                    fs_list[2].write(str(number) + '\n')
+    except IOError as ex:
+        print (ex)
+        print('error when writting document')
+    finally:
+        for fs in fs_list:
+            fs.close()
+    print ('finish operation!')
+
+if __name__ == '__main__':
+    main()
+
+#读文件
+def main():
+    f = None
+    try:
+        f = open('temp.py', 'r', encoding='utf-8')
+        print(f.read())
+    except FileNotFoundError:
+        print('无法打开指定的文件!')
+    except LookupError:
+        print('指定了未知的编码!')
+    except UnicodeDecodeError:
+        print('读取文件时解码错误!')
+    finally:
+        if f:
+            f.close()
+#    try:
+#       with open('致橡树.txt', 'r', encoding='utf-8') as f:
+#          print(f.read())
+
+if __name__ == '__main__':
+    main()
--- temp.py
+++ temp.py
@@ -1,46 +1,92 @@
 
-#图片下载
-from time import time
-from threading import Thread
-
 import requests
-
-
-# 继承Thread类创建自定义的线程类
-class DownloadHanlder(Thread):
-
-    def __init__(self, url):
-        super().__init__()
+import os
+from lxml import etree
+from threading import *
+from time import sleep
+ 
+nMaxThread = 3  #这里设置需要开启几条线程
+ThreadLock = BoundedSemaphore(nMaxThread)
+ 
+gHeads = {
+    "User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36",
+}
+ 
+class Meizitu(Thread):
+    def __init__(self,mainReferer,url,title):
+        Thread.__init__(self)
+        self.MainReferer = mainReferer  
         self.url = url
-
+        self.title = title[3:-4]  #这里是为了把<b></b>给删除
+ 
     def run(self):
-        filename = self.url[self.url.rfind('/') + 1:]
-        resp = requests.get(self.url)
-        with open ('/Downloads/' + filename, 'wb') as f:
-            f.write(resp.content)
-
-
+        try:
+            urlList = self.GetPhotoUrl()
+            if len(urlList) > 0 and urlList != None:
+                self.SavePath(urlList)
+            
+        finally:
+            ThreadLock.release()
+ 
+    def GetPhotoUrl(self):
+        heads={
+            "Referer":self.MainReferer
+        }
+        heads.update(gHeads)
+        html = requests.get(self.url,headers=heads)
+        if html.status_code == 200:
+            xmlContent = etree.HTML(html.text)
+            urlList = xmlContent.xpath("//div[@id='picture']/p/img/@src")
+            return urlList
+        else:
+            return None
+ 
+    def SavePath(self,urlList):
+        heads = {
+            "Referer": self.url
+        }
+        heads.update(gHeads)
+        savePath = "./photo/%s" % self.title
+        if not os.path.exists(savePath):
+            os.makedirs(savePath)
+        for i in range(len(urlList)):
+            j = 0
+            while j<5:
+                print ("Download : %s/%d.jpg" % (self.title.encode("gbk"), i + 1))
+                html = requests.get(urlList[i],headers=heads)
+                if html.status_code == 200:
+                    with open(savePath + "/%d.jpg"%(i+1),"wb") as f:
+                        f.write(html.content)
+                    break
+                elif html.status_code == 404:
+                    j+=1
+                    sleep(0.05)
+                    continue
+                else:
+                    return None
+ 
+ 
 def main():
-    # 通过requests模块的get函数获取网络资源
-    # 下面的代码中使用了天行数据接口提供的网络API
-    # 要使用该数据接口需要在天行数据的网站上注册
-    # 然后用自己的Key替换掉下面代码的中APIKey即可
-    resp = requests.get(
-        'http://api.tianapi.com/meinv/?key=a93ff04ec810984aed7576f4f0d2755a&num=10')
-    # 将服务器返回的JSON格式的数据解析为字典
-    data_model = resp.json()
-    print (data_model)
-    for mm_dict in data_model['newslist']:
-        url = mm_dict['picUrl']
-        # 通过多线程的方式实现图片下载
-        DownloadHanlder(url).start()
-
-
+    while True:
+        try:
+            nNum = int(input("请输入要下载几页: "))
+            if nNum>0:
+                break
+        except ValueError:
+            print("请输入数字。")
+            continue
+    for i in range(nNum):
+        url = "https://www.meizitu.com/a/more_%d.html"%(i+1)
+        html = requests.get(url,headers=gHeads)
+        if html.status_code == 200:
+            xmlContent = etree.HTML(html.content)
+            hrefList = xmlContent.xpath("//div[@class='pic']/a/@href")
+            titleList = xmlContent.xpath("//div[@class='pic']/a/img/@alt")
+            for i in range(len(hrefList)):
+                ThreadLock.acquire()
+                t = Meizitu(url,hrefList[i],titleList[i])
+                t.start()
+ 
+ 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
